2a6d55f7f13f1eb38d6cf81fdf856464ab180861
Ritika Goud <ritikagoud@users.noreply.huggingface.co>
2026-04-08 17:16:26 +0000

    Update server/app.py

--- server/app.py
+++ server/app.py
@@ -118,3 +118,16 @@ def get_state():
 @app.get("/")
 def home():
     return {"message": "SchedulrEnv is running"}
+
+import uvicorn
+
+def main():
+    uvicorn.run(
+        "server.app:app",
+        host="0.0.0.0",
+        port=7860,
+        reload=False
+    )
+
+if __name__ == "__main__":
+    main()
