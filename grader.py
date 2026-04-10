babaaa9f2fa3a13a56d18900038f20d709e885f6
Ritika Goud <ritikagoud@users.noreply.huggingface.co>
2026-04-10 13:03:07 +0000

    Update grader.py

--- grader.py
+++ grader.py
@@ -2,7 +2,6 @@ def grade(states, rewards):
     if not rewards:
         return 0.05
     
-    
     avg_reward = sum(rewards) / len(rewards)
     final_score = float(max(0.05, min(0.95, avg_reward)))
     return final_score
